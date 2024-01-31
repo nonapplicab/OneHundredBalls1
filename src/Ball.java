563503bcdf49b3a67ab804811a53ec2c7e27fa74
fabrizio.paco24 <pacofabrizio@gmail.com>
2024-01-31 08:11:41 -0600

    advanced

--- src/Ball.java
+++ src/Ball.java
@@ -4,29 +4,27 @@ public class Ball {
     private int size;
     private Color coolor;
     private int velocity;
+    private int drop;
     private int xLocation;
     private int yLocation;
-    public Ball (int x,int y){
-        xLocation = x;
-        yLocation = y;
-        size = (int) (Math.random()*100) + 10;
-        coolor = new Color((int)Math.random()*255,(int)Math.random()*255,(int)Math.random()*255);
-        velocity = (int) (Math.random()*1000);
-    }
+
+
     public Ball (JPanel w){
         xLocation = (int)(Math.random()* w.getWidth());
         yLocation = (int)(Math.random()* w.getWidth());
         size = (int) (Math.random()*100) + 10;
         coolor = new Color((int)(Math.random()*255),(int)(Math.random()*255),(int)(Math.random()*255));
-        velocity = (int) (Math.random()*1000);
+        velocity = (int) ((Math.random()-.5)*100);
+        drop = (int) ((Math.random()-.5)*100);
     }
     public void move(int w, int h){
         xLocation+=velocity;
-        if (yLocation+velocity> h ||yLocation+velocity<0 ){
-            velocity= -1*velocity;
+        yLocation+=drop;
+        if (yLocation + drop+ size > h ||yLocation + drop < 0 ){
+            drop= -1 * drop;
         }
-        if (xLocation+velocity> h ||yLocation+velocity<0 ){
-            velocity= -1*velocity;
+        if (xLocation + velocity + size > w || xLocation+velocity < 0 ){
+            velocity= -1 * velocity;
         }
 
     }
--- src/MyPanel.java
+++ src/MyPanel.java
@@ -1,10 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
 public class MyPanel extends JPanel {
-    Ball ballers []= new Ball[20];
+    Ball ballers []= new Ball[100];
 
     public MyPanel(){
         setSize(500,500);
+
         for(int i = 0; i< ballers.length;i++){
             ballers [i] = new Ball(this);
         }
@@ -17,12 +18,12 @@ public class MyPanel extends JPanel {
             ballers[i].draw(g);
             ballers[i].move(getWidth(),getHeight());
             try {
-                Thread.sleep(100);
+                Thread.sleep(10);
             }
             catch(InterruptedException e){
                 System.out.println("do the thing");
             }
-            repaint();
+           repaint();
         }
     }
 }
